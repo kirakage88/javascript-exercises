2051a46e7f20abe358fcf70ba7464714921f7528
kirakage888 <170931512+kirakage88@users.noreply.github.com>
2026-07-19 20:24:27 +0800

    Completed Foundations 11 - tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const factor = 5/9;
+  let answer = (temp-32)*factor
+  let roundedAnswer = Math.ceil(answer* 10)/ 10
+  return roundedAnswer;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const factor = 9/5;
+  let answer = temp*factor + 32;
+  let roundedAnswer = Math.ceil(answer* 10)/ 10
+  return roundedAnswer;
 };
 
 // Do not edit below this line
