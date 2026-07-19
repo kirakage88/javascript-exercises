f06d80d08096f5c393beaec07aeba728d8e6a9fb
kirakage888 <170931512+kirakage88@users.noreply.github.com>
2026-07-19 18:11:23 +0800

    Completed Foundations - 08 removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+  let newArr = [];
+  for (let arrayItem in arr) {
+    if (!items.includes(arr[arrayItem])){
+      newArr.push(arr[arrayItem])
+    }
+  }
+  return newArr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
