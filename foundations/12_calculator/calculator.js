510524fa16319c8076d5273dbdf498dd1393289d
arabellacrumbs-svg <arabella.crumbs@gmail.com>
2026-08-01 16:14:51 +0800

    Completed Calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, current) => acc + current, 0) 
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, current) => acc * current, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {return 1}
+  let array = [];
+  for (let i = num; i > 0; i--) {array.push(i)}
+  return multiply(array)
 };
 
 // Do not edit below this line
