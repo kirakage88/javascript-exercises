e020f575a7ba5355eaa7e650b7099dc0f9ead3ff
kirakage888 <170931512+kirakage88@users.noreply.github.com>
2026-08-02 00:29:33 +0800

    Completed plaindrome JS Exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  clean_str = str.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+  return clean_str === clean_str.split('').reverse().join('')
+  // return clean_str
 };
 
 // Do not edit below this line
