d29dbb61d5759ccef9316261541bb68157638f17
kirakage888 <170931512+kirakage88@users.noreply.github.com>
2026-08-02 00:54:20 +0800

    Completed Fibonacci Exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(limit) {
+let series = [0, 1]
+  if (+limit < 0) {return "OOPS"}
+  if (+limit !== 0) {
+    for (let i = 1; i < +limit; i++) {
+    series.push(series.at(-1)+series.at(-2))
+    }
+  } else {return series.at(-2)}
+  return series.at(-1)
 };
 
 // Do not edit below this line
