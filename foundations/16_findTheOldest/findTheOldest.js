0095d80ea13599f839026e148419aa0326ac6354
kirakage888 <170931512+kirakage88@users.noreply.github.com>
2026-08-02 01:35:38 +0800

    Completed the Find the Oldest Exercise. Complted all foundational JavaScript Exercises. Wooohoo!!!

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  return array.reduce((acc, current) => {
+    if (!('yearOfDeath' in current)) {current['yearOfDeath']=new Date().getFullYear();}
+    if ((acc.yearOfDeath-acc.yearOfBirth) > (current.yearOfDeath-current.yearOfBirth)) {
+      return acc
+    } else {
+      return current
+    }
+  },{yearOfBirth:2005, yearOfDeath: 2005})
 };
 
 // Do not edit below this line
